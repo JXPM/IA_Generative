0db00a2b4d06727f9ba8f0c9fcb275c1a1e57213
JXPM <bilejohan04@gmail.com>
2025-02-26 16:50:52 +0100

    modification de Journal.sh

--- Journal.sh
+++ Journal.sh
@@ -14,5 +14,18 @@ git branch -M main
 git add .
 git commit -m "first commit"
 gh repo create IA_Générative --public
-git remote add origin https://github.com/JXPM/IA_Générative.git
+git remote add origin https://github.com/JXPM/IA_Generative.git
 git push --set-upstream origin main
+
+# Supprimer le fichier .env du suivi git
+git rm --cached .env
+rm '.env'
+
+#create .gitignore
+touch .gitignore
+
+# Ajout du fichier .gitignore
+git add .gitignore
+
+# Commit du fichier .gitignore
+git commit -m "Ajout du fichier .gitignore"
